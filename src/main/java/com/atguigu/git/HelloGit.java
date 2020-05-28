fe26911a50773b92ba1bf9a332a7a643c102039b
chujian <3378226545@qq.com>
2020-05-28 21:09:54 +0800

    第二次提交之前更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -7,6 +7,8 @@ public class HelloGit {
 		System.out.println("Hello 创建分支!");
 		System.out.println("Hello 主线添加!");
 		System.out.println("Hello dev分支!");
-		System.out.println("Hello 改了东西!");
+		System.out.println("Hello 不知道该了东西!");
+		System.out.println("Hello 冲突东西!");
+		System.out.println("Hello 第二次提交之前更新东西!");
 	}
 }
