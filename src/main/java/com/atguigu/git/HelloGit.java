0e54ead454343323ce997cfef90be3df949886f6
chujian <3378226545@qq.com>
2020-05-28 20:46:53 +0800

    主线添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello gengx!!");
 		System.out.println("Hello 创建分支!");
+		System.out.println("Hello 主线添加!");
 	}
 }
