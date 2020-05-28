b8cc1fc77142f319ac589fa988e5f198ae848b39
chujian <3378226545@qq.com>
2020-05-28 20:47:33 +0800

    分支添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello gengx!!");
 		System.out.println("Hello 创建分支!");
+		System.out.println("Hello dev分支!");
+		System.out.println("Hello 改了东西!");
 	}
 }
