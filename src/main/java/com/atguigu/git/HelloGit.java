62175d090a09870582b4d018f153e94ee598624e
chujian <3378226545@qq.com>
2020-05-28 20:44:38 +0800

    创建分支

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello gengx!!");
+		System.out.println("Hello 创建分支!");
 	}
 }
