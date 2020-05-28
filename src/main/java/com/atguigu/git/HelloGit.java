13489adb7735098a45c28be406344c59d5f5bde7
初见 <56104504+Hxinshang@users.noreply.github.com>
2020-05-28 21:08:20 +0800

    在线更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -4,9 +4,11 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello gengx!!");
-		System.out.println("Hello 创建分支!");
-		System.out.println("Hello 主线添加!");
-		System.out.println("Hello dev分支!");
-		System.out.println("Hello 改了东西!");
+		System.out.println("Hello 鍒涘缓鍒嗘敮!");
+		System.out.println("Hello 涓荤嚎娣诲姞!");
+		System.out.println("Hello dev鍒嗘敮!");
+		System.out.println("Hello 鏀逛簡涓滆タ!");
+		System.out.println("Hello 鍦ㄧ嚎娣诲姞!");
+		System.out.println("Hello 鍦ㄧ嚎涓滆タ!");
 	}
 }
